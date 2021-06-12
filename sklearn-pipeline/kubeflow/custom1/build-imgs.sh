15bf9f4acd2eb9e7571a13efc8f24289126fedfd
chris brossman <christopher.brossman@therealreal.com>
2021-06-12 13:11:53 -0700

    adjusted bash files to run

--- sklearn-pipeline/kubeflow/custom1/build-imgs.sh
+++ sklearn-pipeline/kubeflow/custom1/build-imgs.sh
@@ -1,10 +1,12 @@
+source ./setup.sh
 
-./setup.sh
-
+echo ----------------------
 echo BUILDING TRAINER IMAGE
+echo ----------------------
 gcloud builds submit --timeout 15m --tag $TRAINER_IMG trainer_image
 
-
+echo ----------------------
 echo BUILDING BASE IMAGE
+echo ----------------------
 gcloud builds submit --timeout 15m --tag $BASE_IMG base_image
 
--- sklearn-pipeline/kubeflow/custom1/build-pipeline.sh
+++ sklearn-pipeline/kubeflow/custom1/build-pipeline.sh
@@ -1,13 +1,24 @@
 source ./setup.sh
 
 PIPELINE_NAME=covertype_continuous_training
-
 dsl-compile --py pipeline/covertype_training_pipeline.py --output $PIPELINE_NAME.yaml
-# TO DO: Your code goes here
-kfp --endpoint $ENDPOINT pipeline upload -p $PIPELINE_NAME $PIPELINE_NAME.yaml
 
-#GET EXISTING PIPELINE ID
+### Check if PIPELINE already has value - you cant upload a pipeline with same name
 PIPELINE_ID=$(kfp --endpoint $ENDPOINT pipeline list | grep -w $PIPELINE_NAME | tr -s ' ' | cut -d ' ' -f 2 | xargs)
+if [ -z "$PIPELINE_ID" ]
+then
+    echo -------------------
+    echo UPLOADING PIPELINE
+    echo -------------------
+    kfp --endpoint $ENDPOINT pipeline upload -p $PIPELINE_NAME $PIPELINE_NAME.yaml
+    PIPELINE_ID=$(kfp --endpoint $ENDPOINT pipeline list | grep -w $PIPELINE_NAME | tr -s ' ' | cut -d ' ' -f 2 | xargs)
+else 
+    echo -------------------
+    echo PIPELINE ALREADY EXISTS
+    echo -------------------
+fi
+
+
 ### OTHERS
 EXPERIMENT_NAME=Covertype_Classifier_Training
 RUN_ID="RUN_$(date +%Y%m%d_%H%M%S)"
@@ -18,10 +29,11 @@ EVALUATION_METRIC_THRESHOLD=0.69
 MODEL_ID=covertype_classifier
 VERSION_ID=v01
 REPLACE_EXISTING_VERSION=True
+GCS_STAGING_PATH=$ARTIFACT_STORE/staging
 
-GCS_STAGING_PATH=$ARTIFACT_STORE_URI/staging
-
-
+echo -------------------
+echo UPLOADING A RUN
+echo -------------------
 kfp --endpoint $ENDPOINT run submit \
     -e $EXPERIMENT_NAME \
     -r $RUN_ID \
